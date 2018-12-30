8bea44e930c585e503ef28eb642481494e2aaf84
Paul Thompson <paul.thompson@canva.com>
2018-12-30 21:05:36 +1100

    Stop cookies from being added to all requests

--- api/src/main/kotlin/net/devslash/CookieJar.kt
+++ api/src/main/kotlin/net/devslash/CookieJar.kt
@@ -1,21 +1,34 @@
 package net.devslash
 
+import java.net.URL
+
 class CookieJar : SimpleBeforeHook, SimpleAfterHook {
-  private val cookies = mutableMapOf<String, String>()
+  private val cookies = mutableMapOf<URL, MutableMap<String, String>>()
 
   override fun accept(req: HttpRequest, data: RequestData) {
-    val cookie = cookies
-        .map { "${it.key}=${it.value}" }
-        .joinToString("; ")
-    req.addHeader("Cookie", cookie)
+    val basicURl = URL(req.url)
+    val filteredUrl = URL("${basicURl.protocol}://${basicURl.host}")
+    val siteCookies = cookies[filteredUrl]
+
+    siteCookies?.let {
+      val cookie = siteCookies
+          .map { "${it.key}=${it.value}" }
+          .joinToString("; ")
+      req.addHeader("Cookie", cookie)
+    }
   }
 
   override fun accept(resp: HttpResponse) {
+    // we need to sort via the domain name + the security
+    val filteredUrl = URL("${resp.url.protocol}://${resp.url.host}")
     resp.headers.filterKeys { it.equals("Set-Cookie", true) }
         .forEach {
           it.value.forEach { a ->
             val cook = a.split("=")
-            cookies[cook[0]] = cook[1].split(";")[0]
+            if (filteredUrl !in cookies.keys) {
+              cookies[filteredUrl] = mutableMapOf()
+            }
+            cookies[filteredUrl]!![cook[0]] = cook[1].split(";")[0]
           }
         }
   }
--- extensions/src/test/kotlin/net/devslash/CookieJarTest.kt
+++ extensions/src/test/kotlin/net/devslash/CookieJarTest.kt
@@ -1,42 +1,69 @@
 package net.devslash
 
 import net.devslash.util.requestDataFromList
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.nullValue
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.core.IsEqual.equalTo
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.net.URL
 
 internal class CookieJarTest {
 
   private val jar: CookieJar = CookieJar()
-  private val request = HttpRequest(HttpMethod.GET, "", EmptyBodyProvider)
+  private val standardRequest = HttpRequest(HttpMethod.GET, "https://example.com", EmptyBodyProvider)
 
   @Test
   fun testSingleCookieSet() {
     jar.accept(responseWithHeaders(mapOf("Set-Cookie" to listOf("A=B"))))
-    jar.accept(request, requestDataFromList(listOf()))
+    jar.accept(standardRequest, requestDataFromList(listOf()))
 
-    assertThat(request.headers["Cookie"], equalTo(listOf("A=B")))
+    assertThat(standardRequest.headers["Cookie"], equalTo(listOf("A=B")))
   }
 
   @Test fun testMultipleCaseCookieSet() {
     jar.accept(
         responseWithHeaders(mapOf("set-Cookie" to listOf("A=B"), "SET-COOKIE" to listOf("C=D"))))
-    jar.accept(request, requestDataFromList(listOf()))
+    jar.accept(standardRequest, requestDataFromList(listOf()))
 
 
-    assertThat(request.headers["Cookie"], equalTo(listOf("A=B; C=D")))
+    assertThat(standardRequest.headers["Cookie"], equalTo(listOf("A=B; C=D")))
   }
 
   @Test fun testSetMultipleOfTheSameKey() {
     jar.accept(
         responseWithHeaders(mapOf("set-Cookie" to listOf("A=B"), "SET-COOKIE" to listOf("A=D"))))
-    jar.accept(request, requestDataFromList(listOf()))
+    jar.accept(standardRequest, requestDataFromList(listOf()))
 
 
-    assertThat(request.headers["Cookie"], equalTo(listOf("A=D")))
+    assertThat(standardRequest.headers["Cookie"], equalTo(listOf("A=D")))
   }
 
-  private fun responseWithHeaders(headers: Map<String, List<String>>) = HttpResponse(
-      URL("https://example"), 200, headers, "".toByteArray())
+  @Test
+  fun testDomainsDoNotShareCookies() {
+    jar.accept(responseWithHeaders(mapOf("Set-Cookie" to listOf("A=B"))))
+
+    val otherSizeRequest = HttpRequest(HttpMethod.GET, "https://differentDomain.com", EmptyBodyProvider)
+    jar.accept(otherSizeRequest, requestDataFromList(listOf()))
+    jar.accept(standardRequest, requestDataFromList(listOf()))
+
+    assertThat(otherSizeRequest.headers["Cookie"], `is`(nullValue()))
+    assertThat(standardRequest.headers["Cookie"], equalTo(listOf("A=B")))
+  }
+
+  @Test
+  fun testProtocolsDoNotShareCookies() {
+    jar.accept(responseWithHeaders(mapOf("Set-Cookie" to listOf("A=B"))))
+    val httpRequest = HttpRequest(HttpMethod.GET, "http://example.com", EmptyBodyProvider)
+
+    jar.accept(httpRequest, requestDataFromList(listOf()))
+    jar.accept(standardRequest, requestDataFromList(listOf("A=B")))
+  }
+
+  private fun responseWithHeaders(headers: Map<String, List<String>>,
+                                  url: String = "https://example.com/test") = HttpResponse(URL(url),
+      200,
+      headers,
+      "".toByteArray())
 }

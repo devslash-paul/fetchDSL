52c38fc685f144eea6d13cd86d4f83634059a35f
Paul Thompson <paul.thompson@canva.com>
2019-03-05 07:30:10 +1100

    Fix up some appent file stuff

--- extensions/src/main/kotlin/net/devslash/outputs/AppendFile.kt
+++ extensions/src/main/kotlin/net/devslash/outputs/AppendFile.kt
@@ -3,16 +3,26 @@ package net.devslash.outputs
 import net.devslash.*
 import java.io.File
 
-class AppendFile(private val fileName: String, private val out: OutputFormat = DefaultOutput()) :
-  BasicOutput {
+class AppendFile(private val fileName: String,
+                 private val out: OutputFormat = DefaultOutput()) : BasicOutput {
 
   private val lock = Object()
+  private var memoizedFile: File? = null
+
+  init {
+    if(fileName.contains(Regex("!\\d+!"))) {
+      // then it's non memoizable
+      memoizedFile = null
+    } else {
+      memoizedFile = File(fileName)
+    }
+  }
 
   override fun accept(req: HttpRequest, resp: HttpResponse, data: RequestData) {
-    synchronized(lock) {
-      val f = File(fileName.asReplaceableValue().get(data))
-      val output = out.accept(resp, data)
-      if (output != null) {
+    val f = memoizedFile ?: File(fileName.asReplaceableValue().get(data))
+    val output = out.accept(resp, data)
+    if (output != null) {
+      synchronized(lock) {
         f.appendBytes(output)
         f.appendBytes("\n".toByteArray())
       }

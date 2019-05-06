eeb7e30164a1225c8b051b9afb8151560d49e94f
Paul Thompson <paul.thompson@canva.com>
2019-05-06 10:13:25 +1000

    Harden cookie jar

--- api/src/main/kotlin/net/devslash/CookieJar.kt
+++ api/src/main/kotlin/net/devslash/CookieJar.kt
@@ -1,20 +1,24 @@
 package net.devslash
 
 import java.net.URL
+import java.util.concurrent.ConcurrentHashMap
 
 class CookieJar : SimpleBeforeHook, SimpleAfterHook {
-  private val cookies = mutableMapOf<URL, MutableMap<String, String>>()
+  private val cookies = ConcurrentHashMap(mutableMapOf<URL, MutableMap<String, String>>())
 
   override fun accept(req: HttpRequest, data: RequestData) {
     val basicURl = URL(req.url)
     val filteredUrl = URL("${basicURl.protocol}://${basicURl.host}")
-    val siteCookies = cookies[filteredUrl]
 
-    siteCookies?.let {
-      val cookie = siteCookies
-          .map { "${it.key}=${it.value}" }
-          .joinToString("; ")
-      req.addHeader("Cookie", cookie)
+    synchronized(this) {
+      val siteCookies = cookies[filteredUrl]
+
+      siteCookies?.let {
+        // defensive copy
+        val cookies = HashMap(it)
+        val cookie = cookies.map { "${it.key}=${it.value}" }.joinToString("; ")
+        req.addHeader("Cookie", cookie)
+      }
     }
   }
 
@@ -26,7 +30,9 @@ class CookieJar : SimpleBeforeHook, SimpleAfterHook {
           it.value.forEach { a ->
             val cook = a.split("=")
             if (filteredUrl !in cookies.keys) {
-              cookies[filteredUrl] = mutableMapOf()
+              synchronized(this) {
+                cookies[filteredUrl] = ConcurrentHashMap(mutableMapOf())
+              }
             }
             cookies[filteredUrl]!![cook[0]] = cook[1].split(";")[0]
           }

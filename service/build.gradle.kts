97d8dc9199b65cb3d8f68e6f92a51c87f57898ae
Paul Thompson <paulthom12345@gmail.com>
2021-06-20 11:13:10 +1000

    Update jackson dependency, remove unneeded

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -6,24 +6,17 @@ val ktorNettyVersion: String by project
 
 dependencies {
   implementation(kotlin("stdlib", kotlinVersion))
-  implementation(kotlin("reflect", kotlinVersion))
 
   implementation(project(":api"))
 
   implementation("io.ktor:ktor-client-core:$ktorVersion")
   implementation("io.ktor:ktor-client-apache:$ktorVersion")
-
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinxCoroutinesVersion")
+  implementation("com.fasterxml.jackson.core:jackson-core:2.12.3")
+  implementation("com.fasterxml.jackson.core:jackson-databind:2.12.3")
 
-  implementation("com.fasterxml.jackson.core:jackson-core:2.9.8")
-  implementation("com.fasterxml.jackson.core:jackson-databind:2.9.8")
-
-  testImplementation("junit:junit:$junitVersion")
   testImplementation(project(":test-utils"))
-  testImplementation("org.mockito:mockito-core:3.11.1")
+  testImplementation("junit:junit:$junitVersion")
   testImplementation("org.mockito.kotlin:mockito-kotlin:3.2.0")
-  testImplementation("io.ktor:ktor-server-netty:$ktorNettyVersion")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$kotlinxCoroutinesVersion")
-  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug:$kotlinxCoroutinesVersion")
-  testImplementation("org.hamcrest:hamcrest-core:1.3")
 }
--- service/src/test/kotlin/net/devslash/KtorClientAdapterTest.kt
+++ service/src/test/kotlin/net/devslash/KtorClientAdapterTest.kt
@@ -6,6 +6,7 @@ internal class KtorClientAdapterTest {
 
   @Test
   fun testClientMapsParametersSuccessfully() {
+    //TODO
   }
 
 }

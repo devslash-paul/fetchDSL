57fc75e6409a9e20823f40257f44058ece567231
Paul Thompson <paulthom12345@gmail.com>
2022-01-02 19:11:03 +1100

    Very minor changes to cookie manager

--- service/src/main/kotlin/net/devslash/DefaultCookieJar.kt
+++ service/src/main/kotlin/net/devslash/DefaultCookieJar.kt
@@ -1,39 +1,39 @@
 package net.devslash
 
 import java.net.HttpCookie
-import java.net.URL
+import java.net.URI
 import java.util.concurrent.ConcurrentHashMap
 
 class DefaultCookieJar : CookieJar {
   private val cookies = ConcurrentHashMap(mutableMapOf<String, MutableMap<String, String>>())
 
   override fun accept(req: HttpRequest, data: RequestData<*>) {
-    val basicURl = URL(req.url)
-    val filteredUrl = "${basicURl.protocol}://${basicURl.host}"
+    val basicURl = URI(req.url)
+    val filteredUrl = "${basicURl.scheme}://${basicURl.host}"
 
-    synchronized(this) {
-      val siteCookies = cookies[filteredUrl]
-
-      siteCookies?.let { found ->
-        // defensive copy
-        val cookies = found.toList()
-        val cookie = cookies.joinToString("; ") { "${it.first}=${it.second}" }
-        req.addHeader("Cookie", cookie)
-      }
+    val siteCookies = cookies[filteredUrl]
+    siteCookies?.let { found ->
+      // defensive copy
+      val cookies = found.toList()
+      val cookie = cookies.joinToString("; ") { "${it.first}=${it.second}" }
+      req.addHeader("Cookie", cookie)
     }
   }
 
   override fun accept(resp: HttpResponse) {
-    // we need to sort via the domain name + the security
     val filteredUrl = "${resp.uri.scheme}://${resp.uri.host}"
     val setCookie = resp.headers.filterKeys { it.equals("Set-Cookie", true) }
-    setCookie.flatMap { it.value }
-      .map {
-        HttpCookie.parse(it)
+
+    val foundCookies = mutableListOf<HttpCookie>()
+    for (value in setCookie.values) {
+      for (cookie in value) {
+        foundCookies += HttpCookie.parse(cookie)
       }
-      .forEach {
-        val li = cookies.computeIfAbsent(filteredUrl) { ConcurrentHashMap() }
-        li.putAll(it.map { cval -> cval.name to cval.value })
-      }
+    }
+
+    for (cookie in foundCookies) {
+      val li = cookies.computeIfAbsent(filteredUrl) { ConcurrentHashMap() }
+      li[cookie.name] = cookie.value
+    }
   }
 }

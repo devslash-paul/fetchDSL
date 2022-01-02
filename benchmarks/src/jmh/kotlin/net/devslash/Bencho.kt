9346d497282869958f913bb84459921f53e3288e
Paul Thompson <paulthom12345@gmail.com>
2022-01-02 19:31:43 +1100

    Remove extra comments

--- benchmarks/src/jmh/kotlin/net/devslash/Bencho.kt
+++ benchmarks/src/jmh/kotlin/net/devslash/Bencho.kt
@@ -6,26 +6,6 @@ import org.openjdk.jmh.runner.Runner
 import org.openjdk.jmh.runner.options.OptionsBuilder
 import java.util.concurrent.TimeUnit
 
-
-/*
-Result "net.devslash.Bencho.run":
-  1.159 ±(99.9%) 0.001 s/op [Average]
-  (min, avg, max) = (1.157, 1.159, 1.161), stdev = 0.001
-  CI (99.9%): [1.158, 1.159] (assumes normal distribution)
-
- 20-jun-2019
-Result "net.devslash.Bencho.run":
-  1.121 ±(99.9%) 0.002 s/op [Average]
-  (min, avg, max) = (1.118, 1.121, 1.125), stdev = 0.002
-  CI (99.9%): [1.120, 1.123] (assumes normal distribution)
-
-  Benchmark                     Mode  Cnt    Score    Error  Units
-Bencho.runFiveConcurrency    thrpt   10  872.168 ▒ 28.743  ops/s
-Bencho.runTenConcurrency     thrpt   10  448.297 ▒ 26.756  ops/s
-Bencho.runThirtyConcurrency  thrpt   10  445.098 ▒ 21.453  ops/s
-Bencho.runTwentyConcurrency  thrpt   10  447.075 ▒ 29.683  ops/s
- */
-
 @CompilerControl(CompilerControl.Mode.DONT_INLINE)
 @Measurement(iterations = 5, time = 5, timeUnit = TimeUnit.SECONDS)
 @Warmup(iterations = 3, time = 5, timeUnit = TimeUnit.SECONDS)
